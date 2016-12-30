077234f67112132a0cc66f86b715d75267956776
Alfredo Henriquez <ahenriquez@sodired.cl>
2016-12-30 18:31:52 -0300

    Modified: DocPersonal

--- documentacionPersonal.php
+++ documentacionPersonal.php
@@ -121,6 +121,7 @@ $nombreUsuario = $_SESSION['nombreUsuario'];
 	<link rel="stylesheet" type="text/css" href="css/new-article.css">
 	<link rel="stylesheet" type="text/css" href="css/documentacionPersonal.css">
 
+
 	<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
