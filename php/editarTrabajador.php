542d2f49b5c07b41a061fd929f4c66774dd0af99
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-01-06 17:49:40 -0300

    Modified: Editar persona fixed

--- php/editarTrabajador.php
+++ php/editarTrabajador.php
@@ -1,13 +1,13 @@
 <?php
 	include('conexion.php');
 	$id=$_POST['id'];
-	$nombre = $_POST['nombre'];		
+	$nombre = $_POST['nombre'];
 	$rut = $_POST['rut'];
 	$apellidos = $_POST['apellidos'];
 	$fnac = $_POST['fnac'];
 	$sexo = $_POST['sexo'];
 	$nacionalidad = $_POST['nacionalidad'];
-	$visa = $_POST['visa'];
+	$visa = $_POST['id_visa'];
 	$procedencia = $_POST['procedencia'];
 	$cargo = $_POST['cargo'];
 	$direccion = $_POST['direccion'];
@@ -15,10 +15,47 @@
 	$alergias = $_POST['alergias']; 
 	$id_grupo_sanguineo = $_POST['id_grupo_sanguineo']; 
 	$id_tipo_pase = $_POST['id_tipo_pase']; 
-	$id_tipo_turno = $_POST['id_tipo_turno'];                 
+	$id_tipo_turno = $_POST['id_tipo_turno'];  
+	$tipocontrato = $_POST['id_tipo_contrato'];  
+	$fvencvisa = $_POST['fvencvisa'];
+	$regionid = $_POST['region_id'];
+	$fechainicio = $_POST['fechainicio'];
+	$fechatermino = $_POST['fechatermino'];
+	$iniciopase = $_POST['iniciopase'];
+	$terminopase = $_POST['terminopase'];
+	$sector = $_POST['id_sector'];
+
+
 	$con = conectarse();
 	mysql_set_charset("utf8",$con);
-	$sql = "UPDATE personal_acreditado SET RUT='$rut', nombres='$nombre', apellidos='$apellidos', f_nacimiento='$fnac', id_sexo='$sexo', nacionalidad='$nacionalidad', visa='$visa', procedencia='$procedencia', cargo='$cargo', direccion='$direccion', fono_emergencia='$fono', alergias='$alergias', id_grupo_sanguineo='$id_grupo_sanguineo', id_tipo_pase='$id_tipo_pase', id_tipo_turno='$id_tipo_turno', f_registro='now()' WHERE id_acreditado='$id'";
+
+	$sql = "
+	UPDATE personal_acreditado 
+	SET 
+	nombres='$nombre', 
+	apellidos='$apellidos', 
+	f_nacimiento='$fnac', 
+	id_sexo='$sexo', 
+	nacionalidad='$nacionalidad', 
+	id_visa='$visa', 
+	procedencia='$procedencia', 
+	cargo='$cargo', 
+	direccion='$direccion', 
+	fono_emergencia='$fono', 
+	alergias='$alergias', 
+	id_grupo_sanguineo='$id_grupo_sanguineo', 
+	id_tipo_pase='$id_tipo_pase', 
+	id_tipo_turno='$id_tipo_turno', 
+	f_registro='now()', 
+	fvencvisa='$fvencvisa', 
+	region_id='$regionid', 
+	id_tipo_contrato='$tipocontrato',
+	fechainicio = '$fechainicio',
+	fechatermino = '$fechatermino',
+	id_sector = '$sector'
+	WHERE 
+	id_acreditado='$id'";
+
 	mysql_query($sql,$con);
 	header("location: http://www.chilecop.cl/acreditacion/editarPersonal.php?id=$id");
 	mysql_close($con);
