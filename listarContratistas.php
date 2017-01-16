863b8d034ecdba1368f70dd55d4764b0f2cc262c
Alfredo Henriquez <ahenriquez@sodired.cl>
2017-01-16 13:43:42 -0300

    Modified: Restricción de rut al ingresar un trabajador

--- listarContratistas.php
+++ listarContratistas.php
@@ -60,7 +60,7 @@ if($_SESSION['nombreUsuario'] && $nombreUsuario!="Contratista"){
 	          <div id="content">
 	            <header class="clearfix">
 	              <div class="col-xs-5 col-sm-3 col-md-3"><b>Empresas Contratistas</b></div>
-	              <?php if($usuario =="Admin"){ ?>
+	              <?php if($nombreUsuario =="Admin"){ ?>
 	              <div class="col-xs-7 col-sm-9 col-md-9">
 	              	<a class="btn btn-xs btn-success pull-right" href="ingresarContratista.php" role="button">Ingresar Empresa</a>	
 	              </div>
--- php/addTrabajador.php
+++ php/addTrabajador.php
@@ -23,6 +23,7 @@
 	$fechatermino = $_POST['fechatermino'];
 	$iniciopase = $_POST['iniciopase'];
 	$jornada = $_POST['id_registro'];
+	$empresa = $_POST['empresa'];
 	$con = conectarse();
 	mysql_set_charset("utf8",$con);
 
@@ -30,35 +31,45 @@
 	* Antes de ingresar un trabajador, buscamos la caducidad del contrato
 	* y así se define el termino de pase con la misma fecha de este
 	*/
-	$sql = "SELECT F_TERMINO FROM orden_contrato WHERE ID_OC='$idcontrato'";
+	$sql = "SELECT oc.F_TERMINO, pa.RUT FROM orden_contrato oc, personal_acreditado pa WHERE ID_OC='$idcontrato' AND pa.RUT='$rut'";
 	$resultado = mysql_query($sql,$con);
 	$fila = mysql_fetch_array($resultado);
 
-	$sql = "INSERT INTO personal_acreditado (id_estado, id_orden_contrato, rut, nombres, apellidos, f_nacimiento, id_sexo, nacionalidad, id_visa, procedencia, cargo, direccion, fono_emergencia, id_tipo_contrato, id_tipo_pase, id_tipo_turno, region_id, fechainicio, fechatermino, terminopase, id_sector, fVencVisa, f_registro, tipo_jornada) VALUES ('3', '$idcontrato', '$rut', '$nombre', '$apellidos', '$fnac', '$sexo', '$nacionalidad', '$id_visa', '$procedencia','$cargo', '$direccion', '$fono', '$id_tipo_contrato','$id_tipo_pase','$id_tipo_turno','$region_id', '$fechainicio', '$fechatermino','". $fila['F_TERMINO'] . "','$id_sector', '$fVencVisa', now(), '$jornada')";
-	$resultado = mysql_query($sql,$con);
+	if(!$fila['RUT']){	
 
-	if($resultado){
-		//CREACIÓN DE LA CARPETA PARA LOS ARCHIVOS CORRESPONDIENTES
-		$directorio = "../archivos/" . $rut;
-		mkdir($directorio, 0777);
-
-
-		/**
-		 * CREACIÓN DE LA INSTANCIA PARA LA DOCUMENTACIÓN
-		 */		
-		//PRIMERO CAPTURAMOS EL ID DEL RECIEN INSERTADO
-		$sql = "SELECT ID_ACREDITADO FROM personal_acreditado GROUP BY ID_ACREDITADO DESC LIMIT 1";
+		$sql = "INSERT INTO personal_acreditado (id_estado, id_orden_contrato, rut, nombres, apellidos, f_nacimiento, id_sexo, nacionalidad, id_visa, procedencia, cargo, direccion, fono_emergencia, id_tipo_contrato, id_tipo_pase, id_tipo_turno, region_id, fechainicio, fechatermino, terminopase, id_sector, fVencVisa, f_registro, tipo_jornada) VALUES ('3', '$idcontrato', '$rut', '$nombre', '$apellidos', '$fnac', '$sexo', '$nacionalidad', '$id_visa', '$procedencia','$cargo', '$direccion', '$fono', '$id_tipo_contrato','$id_tipo_pase','$id_tipo_turno','$region_id', '$fechainicio', '$fechatermino','". $fila['F_TERMINO'] . "','$id_sector', '$fVencVisa', now(), '$jornada')";
 		$resultado = mysql_query($sql,$con);
-		$fila = mysql_fetch_array($resultado);
-		$acreditado = $fila['ID_ACREDITADO'];
-		//AHORA CREAMOS LA INSTANCIA	
-		$sql = "INSERT INTO documentacion (ID_ACREDITADO) VALUES ($acreditado)"; 
-		mysql_query($sql,$con);
-		//RETORNAMOS A LA PAGINA DE LOS CONTRATOS
-		$_SESSION['mensajeAlerta'] = "Personal ingresado correctamente. Ahora, debe ingresar los documentos de " . $nombre . " " . $apellidos . ". Puede ingresarlos inmediatamente, o bien paulatinamente volviendo a esta ventana, sin embargo, al ingresar un documento, no podrá modificarlo posteriormete.";
-		header("location: http://www.chilecop.cl/acreditacion/documentacionPersonal.php?id=$acreditado");
-		mysql_close($con);
+
+		if($resultado){
+			//CREACIÓN DE LA CARPETA PARA LOS ARCHIVOS CORRESPONDIENTES
+			$directorio = "../archivos/" . $rut;
+			mkdir($directorio, 0777);
+
+
+			/**
+			 * CREACIÓN DE LA INSTANCIA PARA LA DOCUMENTACIÓN
+			 */		
+			//PRIMERO CAPTURAMOS EL ID DEL RECIEN INSERTADO
+			$sql = "SELECT ID_ACREDITADO FROM personal_acreditado GROUP BY ID_ACREDITADO DESC LIMIT 1";
+			$resultado = mysql_query($sql,$con);
+			$fila = mysql_fetch_array($resultado);
+			$acreditado = $fila['ID_ACREDITADO'];
+			//AHORA CREAMOS LA INSTANCIA	
+			$sql = "INSERT INTO documentacion (ID_ACREDITADO) VALUES ($acreditado)"; 
+			mysql_query($sql,$con);
+			//RETORNAMOS A LA PAGINA DE LOS CONTRATOS
+			$_SESSION['mensajeAlerta'] = "Personal ingresado correctamente. Ahora, debe ingresar los documentos de " . $nombre . " " . $apellidos . ". Puede ingresarlos inmediatamente, o bien paulatinamente volviendo a esta ventana, sin embargo, al ingresar un documento, no podrá modificarlo posteriormete.";
+			header("location: http://www.chilecop.cl/acreditacion/documentacionPersonal.php?id=$acreditado");
+			mysql_close($con);
+		}else{
+			header("location: http://www.chilecop.cl/acreditacion/listarPersonal.php?id=$idcontrato");
+		}
 	}else{
-		header("location: http://www.chilecop.cl/acreditacion/listarPersonal.php?id=$idcontrato");
-	}	
+		$_SESSION['mensajeAlerta']="Persona con rut " . $rut . " ya existente.";
+		if($empresa){
+			header("location: http://www.chilecop.cl/acreditacion/ingresarTrabajador2.php?id=$idcontrato");
+		}else{
+			header("location: http://www.chilecop.cl/acreditacion/ingresarTrabajador.php?id=$idcontrato");
+		}		
+	}
 ?>
